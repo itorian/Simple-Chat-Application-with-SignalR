65508928c1d38bddc744e90da53768a26686c967
itorian <abhimanyukumarvatsa@yahoo.co.in>
2015-04-06 11:44:34 +0530

    init

---
+++ ChattingApplication/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChattingApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+
+    public class ChatHub : Microsoft.AspNet.SignalR.Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
